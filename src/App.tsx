ba4cc034fba80393e12d7d85b15110267bdb1866
victorgalvezsma <victor.galvez@smasoftware.com>
2024-02-19 15:16:17 -0500

    clean typescript warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { CSSProperties, useEffect, useState } from "react";
 import {
   AgoraRTCProvider,
@@ -18,7 +19,6 @@ import io from 'socket.io-client';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import CommentIcon from '@mui/icons-material/Comment';
 import IconButton from '@mui/material/IconButton';
 import { PhoneForwarded } from "@mui/icons-material";
 import { Box, TextField } from "@mui/material";
